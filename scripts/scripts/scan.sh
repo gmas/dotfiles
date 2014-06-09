0c7f9e11f531f786eca3cb2e409520afe0b18742
gmas <gmasgras@gmail.com>
2014-06-08 18:14:17 -0700

    create tmp files in separate dir

--- scripts/scripts/scan.sh
+++ scripts/scripts/scan.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "NAME: $NAME | Page count: $COUNT"
+BASEDIR=/nas/scans/unsorted
+TMPDIR=$BASEDIR/$NAME"_tmp"
 
-/usr/bin/scanimage --resolution 150 --mode Gray --button-controlled=yes --format=tiff --batch-count "$COUNT" --batch="/nas/scans/unsorted/$NAME-%d.tiff" 
-echo "Finished scanning"
+/bin/mkdir -p $TMPDIR
+/usr/bin/scanimage --resolution 150 --mode Gray --button-controlled=yes --format=tiff --batch-count "$COUNT" --batch="$TMPDIR"/"$NAME-%d.tiff" 
+echo "Finished scanning to $TMPDIR"
 
-echo "Converting to PDF: $NAME.pdf"
-#/usr/bin/convert -resize 1276x1754 "/nas/scans/unsorted/$NAME-*" /nas/scans/unsorted/$NAME.pdf
-/usr/bin/convert "/nas/scans/unsorted/$NAME-*" /nas/scans/unsorted/$NAME.pdf
+echo "Converting to PDF: $BASEDIR/$NAME.pdf"
+#/usr/bin/convert -resize 1276x1754 "/nas/scans/unsorted/$NAME_tmp/$NAME-*" /nas/scans/unsorted/$NAME.pdf
+/usr/bin/convert $TMPDIR/$NAME-*.tiff /nas/scans/unsorted/$NAME.pdf
 
