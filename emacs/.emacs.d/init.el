cf7db3bcfb7487d8805c492a06dc87debc964c5c
George Masgras <gmasgras@gmail.com>
2016-07-19 18:22:29 -0700

    move customizations to custom.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,7 @@
+(setq dotfiles-dir "~/.emacs.d/")
+(setq custom-file (concat dotfiles-dir "custom.el"))
+(load custom-file)
+
 ;; Backups location
 (defvar backup-dir "~/.emacs.d/backups/")
 ;; (setq backup-directory-alist (list (cons "." backup-dir)))
@@ -72,20 +76,6 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
-;set before elscreen-start
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(elscreen-tab-background-face ((t (:background "gray90"))))
- '(elscreen-tab-control-face ((t (:background "gray90" :foreground "gray60"))))
- '(elscreen-tab-current-screen-face ((t (:background "brown" :foreground "white"))))
- '(elscreen-tab-other-screen-face ((t (:background "gray10" :foreground "gray50"))))
- '(flycheck-color-mode-line-error-face ((t (:inherit flycheck-fringe-error :foreground "brightred" :weight semi-bold))))
- '(flycheck-error ((t (:inherit (error default) :underline "red"))))
- '(flycheck-error-list-error ((t (:inherit error))))
- '(flycheck-error-list-highlight ((t (:inherit highlight :underline t)))))
 
 ;ElScreen
 (use-package elscreen
@@ -150,24 +140,6 @@
   :bind ("C-s" . swiper))
 (ivy-mode 1)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(coffee-indent-tabs-mode nil)
- '(coffee-tab-width 2)
- '(custom-safe-themes
-   (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "9ff70d8009ce8da6fa204e803022f8160c700503b6029a8d8880a7a78c5ff2e5" "6c62b1cd715d26eb5aa53843ed9a54fc2b0d7c5e0f5118d4efafa13d7715c56e" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" "40c66989886b3f05b0c4f80952f128c6c4600f85b1f0996caa1fa1479e20c082" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(enh-ruby-indent-tabs-mode nil)
- '(evil-shift-width 2)
- '(js-indent-level 2)
- '(jsx-indent-level 2)
- '(jsx-use-auto-complete t)
- '(ruby-align-chained-calls t)
- '(ruby-align-to-stmt-keywords (quote (begin if def)))
- '(ruby-indent-tabs-mode nil))
 
 (load-theme 'afternoon)
 
