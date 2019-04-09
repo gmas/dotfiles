042d58a420875c23f810104101daf3925df992f0
George Masgras <gmasgras@gmail.com>
2019-04-09 13:26:41 -0700

    [emacs] add dired-subtree

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -430,4 +430,10 @@
     (modify-syntax-entry ?_ "w" table)
     (with-syntax-table table
       ad-do-it)))
+
+(use-package dired-subtree :ensure t
+  :after dired
+  :config
+  (bind-key "<tab>" #'dired-subtree-toggle dired-mode-map)
+  (bind-key "<backtab>" #'dired-subtree-cycle dired-mode-map))
 ;;; init.el ends here
