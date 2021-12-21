045c43a7f5ce748c6933a01fc0090708323890eb
gmas <gmasgras@gmail.com>
2021-12-21 13:32:36 -0800

    re-enable mouse scrolling. unbind quit key. add text scaling keys

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -506,11 +506,11 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Disable mouse wheel (and two finger swipe) scrolling
-(mouse-wheel-mode -1)
-(global-set-key [wheel-up] 'ignore)
-(global-set-key [wheel-down] 'ignore)
-(global-set-key [double-wheel-up] 'ignore)
-(global-set-key [double-wheel-down] 'ignore)
+;;(mouse-wheel-mode -1)
+;;(global-set-key [wheel-up] 'ignore)
+;;(global-set-key [wheel-down] 'ignore)
+;;(global-set-key [double-wheel-up] 'ignore)
+;;(global-set-key [double-wheel-down] 'ignore)
 (global-set-key [triple-wheel-up] 'ignore)
 (global-set-key [triple-wheel-down] 'ignore)
 (global-set-key [wheel-left] 'ignore)
@@ -653,3 +653,10 @@
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
+
+(global-unset-key (kbd "C-x C-c"))
+
+;; Shift + scroll to change font size
+(global-set-key [S-mouse-4] 'text-scale-increase)
+(global-set-key [S-mouse-5] 'text-scale-decrease)
+;;; (ignore-errors (run-with-timer 0 180 'forge-pull))
