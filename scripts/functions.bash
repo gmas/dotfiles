f2e4c7249c05380d9f380edf2281cd243219aa83
gmas <gmasgras@gmail.com>
2021-12-21 13:55:32 -0800

    add bash function for removing known ssh host by line number

--- scripts/functions.bash
+++ scripts/functions.bash
@@ -1,3 +1,7 @@
 function used_ram {
   ps axu | grep -i "$1" | awk 'BEGIN {FS = " "} ; {sum+=$6} END {print sum}' | gnumfmt --to=iec --from-unit=1024
 }
+
+function ssh_rem_known {
+  sed -i -e "$1d" ~/.ssh/known_hosts
+}
